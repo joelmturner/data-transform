5f5d4006f7422201273b273e6e2d6b866e9da881
joelmturner <joel@joelmturner.com>
2023-01-21 05:40:02 -0800

    final code

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { getCityAverages } from "./getCityAverages";
+import { getCityAverages } from "./getCityAverages-final";
 import { cityData } from "./fixtureData";
 
 function App() {
-  return (
-    <div className="App">
-      <pre>{JSON.stringify(getCityAverages(cityData), null, 2)}</pre>
-    </div>
-  );
+    return (
+        <div className="App">
+            <pre>{JSON.stringify(getCityAverages(cityData), null, 2)}</pre>
+        </div>
+    );
 }
 
 export default App;
